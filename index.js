9b560e56a916f5fc08f59a017fa3756eadcdf48a
Si <si@Prometheus.lan>
2021-04-01 00:26:21 +0100

    ✨ add random factor to server ping

--- index.js
+++ index.js
@@ -14,11 +14,48 @@ var options = {
 
 const prefix = `!!`;
 
-const channels = [];
+const getQuote = () => quotes[getRandomInt(0, quotes.length)];
 
+const getOnlineQuote = async () => {
+  try {
+    const response = await axios.request(options);
+    // console.log(repsonse.data);
+    const { a, q } = response.data[0];
+    return `"${q}" - ${a}`;
+  } catch (err) {
+    console.error(err);
+  }
+
+  return ``;
+}
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+const timeToMs = (hours = 0, minutes = 0, seconds = 0, ms = 0) => {
+  let time = 0;
+  time += ms;
+  time += seconds * 1000;
+  time += minutes * 1000 * 60;
+  time += hours * 1000 * 60 * 24;
+  return time;
+}
+
+// ------------ events
 
 client.on('ready', async () => {
+  const random = getRandomInt(0, 2);
+  console.log(random);
   // List servers the client is connected to
+  if([1].includes(random)){
+    pingServers();
+  }
+});
+
+const pingServers = async () => {
   console.log("Servers:")
   client.guilds.cache.forEach((guild) => {
     console.log(" - " + guild.name);
@@ -47,7 +84,7 @@ client.on('ready', async () => {
     })
     sendMessage(positivityChannelId);
   });
-});
+}
 
 const sendMessage = async channelId => {
   try {
@@ -70,27 +107,20 @@ client.on(`message`, async msg => {
 
   if (command === "quote") {
     console.log(`quote`);
+    msg.channel.send(await getOnlineQuote());
+  }
+
+  if (command === "inspire") {
+    console.log(`quote`);
     msg.channel.send(getQuote());
   }
 });
 
-const getQuote = () => quotes[getRandomInt(0, quotes.length)];
+// setInterval(() => {
+//   const random = getRandomInt(0, 60);
+//   console.log(random);
+//   if([25, 33, 57, 20, 1].includes(random)){
+//     pingServers();
+//   }
+// }, timeToMs(0, 0, 1, 0));
 
-const getOnlineQuote = async () => {
-  try {
-    const response = await axios.request(options);
-    // console.log(repsonse.data);
-    const { a, q } = response.data[0];
-    return `"${q}" - ${a}`;
-  } catch (err) {
-    console.error(err);
-  }
-
-  return ``;
-}
-
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
