5484be9919c5b8d199bca447f807a8b287fd3686
Si <syblade25@gmail.com>
2021-04-02 01:23:45 +0100

    🚀 update for deployment in GCP

--- index.js
+++ index.js
@@ -1,107 +1,97 @@
-// ------------ import modules
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
 
-require('dotenv').config();
-const Discord = require('discord.js');
-const quotes = require('./quotes.json');
-const axios = require("axios").default;
-const client = new Discord.Client();
+const pingDiscord = () => {
+  require('dotenv').config();
+  const Discord = require('discord.js');
+  const quotes = require('./quotes.json');
+  const axios = require("axios").default;
+  const client = new Discord.Client();
 
-// ------------ log in client
+  // ------------ log in client
 
-client.login(process.env.AUTH_TOKEN);
+  client.login(process.env.AUTH_TOKEN);
 
-// ------------ consts
+  // ------------ utility functions 
 
-const options = {
-  method: `GET`,
-  url: `https://zenquotes.io/api/random`,
-};
+  const getQuote = () => quotes[getRandomInt(0, quotes.length)];
 
-const prefix = `!!`;
-
-// ------------ utility functions 
-
-const getQuote = () => quotes[getRandomInt(0, quotes.length)];
-
-const getOnlineQuote = async () => {
-  try {
-    const response = await axios.request(options);
-    // console.log(repsonse.data);
-    const { a, q } = response.data[0];
-    return `"${q}" - ${a}`;
-  } catch (err) {
-    console.error(err);
+  // TODO: Write unit tests for this
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
-  return ``;
-}
+  // ------------ messaging functions
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
+  const pingServers = async () => {
+    console.log("Servers:")
+    client.guilds.cache.forEach((guild) => {
+      console.log(" - " + guild.name);
+      let positivityChannelId;
 
-// ------------ messaging functions
-
-const pingServers = async () => {
-  console.log("Servers:")
-  client.guilds.cache.forEach((guild) => {
-    console.log(" - " + guild.name);
-    let positivityChannelId;
-
-    // List all channels
-    guild.channels.cache.forEach(channel => {
-      console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-      if (channel.type === `text`) {
-        if (channel.name === `general` && positivityChannelId === undefined) {
-          positivityChannelId = channel.id;
-        }
-        if (channel.name === `positivity`) {
-          positivityChannelId = channel.id;
-        }
+      if (process.env.DEV === `true` && guild.name === `Sy's server` || process.env.DEV === undefined) {
+        // List all channels
+        guild.channels.cache.forEach(channel => {
+          channel.type === `text` && console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+          if (channel.type === `text`) {
+            if (channel.name === `general` && positivityChannelId === undefined) {
+              positivityChannelId = channel.id;
+            }
+            if (channel.name === `positivity`) {
+              positivityChannelId = channel.id;
+            }
+          }
+        });
+        sendMessage(positivityChannelId);
       }
-    })
-    sendMessage(positivityChannelId);
+    });
+  }
+
+  const sendMessage = async channelId => {
+    try {
+      const channel = await client.channels.fetch(channelId);
+      const quote = getQuote();
+      console.log(`sending to ${channelId}: ${quote}`);
+      channel.send(quote);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // ------------ event handlers
+
+  client.on('ready', async () => {
+    const random = getRandomInt(0, 2);
+    console.log(random);
+    // List servers the client is connected to
+    // if([1].includes(random)){
+    pingServers();
+    // }
   });
 }
 
-const sendMessage = async channelId => {
-  try {
-    const channel = await client.channels.fetch(channelId);
-    channel.send(getQuote());
-  } catch (err) {
-    console.log(err);
+exports.pingDiscord = (event, context) => {
+  pingDiscord();
+
+  const message = event.data
+    ? Buffer.from(event.data, 'base64').toString()
+    : 'Ping triggered';
+  console.log('Ping triggered');
+};
+
+// A bit of a hack to run a local version of the function
+// better way would be separate the Discord code into another file and import into the two exec points
+const executeDev = () => {
+  require('dotenv').config();
+  if (process.env.DEV === `true`) {
+    pingDiscord();
   }
 }
 
-// ------------ event handlers
-
-client.on('ready', async () => {
-  const random = getRandomInt(0, 2);
-  console.log(random);
-  // List servers the client is connected to
-  if([1].includes(random)){
-    pingServers();
-  }
-});
-
-client.on(`message`, async msg => {
-  if (msg.author.bot) return;
-
-  if (!msg.content.startsWith(prefix)) return;
-
-  const commandBody = msg.content.slice(prefix.length);
-  const args = commandBody.split(` `);
-  const command = args.shift().toLowerCase();
-
-  if (command === "quote") {
-    console.log(`quote`);
-    msg.channel.send(await getOnlineQuote());
-  }
-
-  if (command === "inspire") {
-    console.log(`quote`);
-    msg.channel.send(getQuote());
-  }
-});
+executeDev();
---
+++ original-code.js
@@ -0,0 +1,112 @@
+// ------------ import modules
+
+require('dotenv').config();
+const Discord = require('discord.js');
+const quotes = require('./quotes.json');
+const axios = require("axios").default;
+const client = new Discord.Client();
+
+// ------------ log in client
+
+client.login(process.env.AUTH_TOKEN);
+
+// ------------ consts
+
+const options = {
+  method: `GET`,
+  url: `https://zenquotes.io/api/random`,
+};
+
+const prefix = `!!`;
+
+// ------------ utility functions 
+
+const getQuote = () => quotes[getRandomInt(0, quotes.length)];
+
+const getOnlineQuote = async () => {
+  try {
+    const response = await axios.request(options);
+    // console.log(repsonse.data);
+    const { a, q } = response.data[0];
+    return `"${q}" - ${a}`;
+  } catch (err) {
+    console.error(err);
+  }
+
+  return ``;
+}
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+// ------------ messaging functions
+
+const pingServers = async () => {
+  console.log("Servers:")
+  client.guilds.cache.forEach((guild) => {
+    console.log(" - " + guild.name);
+    let positivityChannelId;
+
+    // List all channels
+    guild.channels.cache.forEach(channel => {
+      console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+      if (channel.type === `text`) {
+        if (channel.name === `general` && positivityChannelId === undefined) {
+          positivityChannelId = channel.id;
+        }
+        if (channel.name === `positivity`) {
+          positivityChannelId = channel.id;
+        }
+      }
+    })
+    sendMessage(positivityChannelId);
+  });
+}
+
+const sendMessage = async channelId => {
+  try {
+    const channel = await client.channels.fetch(channelId);
+    channel.send(getQuote());
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// ------------ event handlers
+
+client.on('ready', async () => {
+  const random = getRandomInt(0, 2);
+  console.log(random);
+  // List servers the client is connected to
+  if([1].includes(random)){
+    pingServers();
+  }
+});
+
+client.on(`message`, async msg => {
+  if (msg.author.bot) return;
+
+  if (!msg.content.startsWith(prefix)) return;
+
+  const commandBody = msg.content.slice(prefix.length);
+  const args = commandBody.split(` `);
+  const command = args.shift().toLowerCase();
+
+  if (command === "quote") {
+    console.log(`quote`);
+    msg.channel.send(await getOnlineQuote());
+  }
+
+  if (command === "inspire") {
+    console.log(`quote`);
+    msg.channel.send(getQuote());
+  }
+});
+
+exports.helloWorld = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
