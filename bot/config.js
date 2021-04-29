92f6d732182206a0f25fc150a9a58e34318c4fb0
Si <syblade25@gmail.com>
2021-04-30 00:08:44 +0100

    🔧 reduce chance of message to be sent

--- bot/config.js
+++ bot/config.js
@@ -1,7 +1,7 @@
 const quotes = require('./data/quotes.json');
 
 module.exports = {
-  RANDOM_MAX_VALUE_FOR_SEND_CHANCE: 12,
+  RANDOM_MAX_VALUE_FOR_SEND_CHANCE: 16,
   LOCALE: `GMT`,
   OPERATING_HOURS_START: 6,
   OPERATING_HOURS_END: 23,
