ff1fbe5f4781cc79a3ddcd48b90e5efb8ed81d8f
Si <syblade25@gmail.com>
2021-05-05 00:14:54 +0100

    ✨ WIP add prep for Firestore integration

---
+++ bot/firestore.js
@@ -0,0 +1,3 @@
+// this is where all the read-write for firestore will happen
+// I need to see if I can simulate a firestore action or otherwise return a default config if firestore is not present
+// There is a firebase with firestore emulator, but I want to avoid using that so U can keep this tight
